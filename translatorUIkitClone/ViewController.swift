d804d276c9873c44df6773f6b9f4dad3ea405b00
Sunho <sunh803@naver.com>
2023-05-20 17:17:51 +0900

    Initial Commit

---
+++ translatorUIkitClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  translatorUIkitClone
+//
+//  Created by 235 on 2023/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
